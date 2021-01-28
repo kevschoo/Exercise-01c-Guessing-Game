f3beb18f288c7f538b297ae4d7e20112111c40e3
kevschoo <kevschoo@iu.edu>
2021-01-28 01:13:07 -0500

    Final

--- main2.py
+++ main2.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 import sys
 assert sys.version_info >= (3,9), "This script requires at least Python 3.9"
+import random
+
+number = random.randrange(1,11)
+guess = input("Guess a number from 1 to 10: ")
+if guess.isdigit():
+    guess = int(guess)
+        
+    if guess == number:
+        print("Great job! You got it!")
+    else:
+        print("Sorry, better luck next time.")
+        
+        print("The number was " + str(number))
+else:
+    print(guess + " Is not a number")
